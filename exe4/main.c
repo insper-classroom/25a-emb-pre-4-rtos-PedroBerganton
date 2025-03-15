9106c6e5a0779085754bec37785156470a4c90cd
Pedro Berganton <pberganton@hotmail.com>
2025-03-15 13:56:38 -0300

    Ex4.3

--- exe4/main.c
+++ exe4/main.c
@@ -16,10 +16,18 @@ QueueHandle_t xQueueButId;
 SemaphoreHandle_t xSemaphore_r;
 
 QueueHandle_t xQueueButId_G;
-SemaphoreHandle_t xSemaphore_G;
+
 void btn_callback_G(uint gpio, uint32_t events) {
     if (events == 0x4) { // fall edge
-        xSemaphoreGiveFromISR(xSemaphore_G, 0);
+
+        static int delay_G = 0;
+        if (delay_G<1000){
+            delay_G+=100;
+        } else{
+            delay_G =100;
+        }
+        //isso envia direot para fila do L.G
+        xQueueSendFromISR(xQueueButId_G, &delay_G, NULL);
     }
 }
 void btn_callback(uint gpio, uint32_t events) {
@@ -69,26 +77,6 @@ void btn_1_task(void *p) {
     }
 }
 
-void btn_2_task(void *p) {
-    gpio_init(BTN_PIN_G);
-    gpio_set_dir(BTN_PIN_G, GPIO_IN);
-    gpio_pull_up(BTN_PIN_G);
-    gpio_set_irq_enabled_with_callback(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true,
-                                       &btn_callback_G);
-
-    int delay = 0;
-    while (true) {
-        if (xSemaphoreTake(xSemaphore_G, pdMS_TO_TICKS(500)) == pdTRUE) {
-            if (delay < 1000) {
-                delay += 100;
-            } else {
-                delay = 100;
-            }
-            printf("delay btn %d \n", delay);
-            xQueueSend(xQueueButId_G, &delay, 0);
-        }
-    }
-}
 
 void led_2_task(void *p) {
     gpio_init(LED_PIN_G);
@@ -118,13 +106,18 @@ int main() {
     xSemaphore_r = xSemaphoreCreateBinary();
 
     xQueueButId_G = xQueueCreate(32, sizeof(int));
-    xSemaphore_G = xSemaphoreCreateBinary();
 
     xTaskCreate(led_1_task, "LED_Task 1", 256, NULL, 1, NULL);
     xTaskCreate(btn_1_task, "BTN_Task 1", 256, NULL, 1, NULL);
-
     xTaskCreate(led_2_task, "LED_Task 2", 256, NULL, 1, NULL);
-    xTaskCreate(btn_2_task, "BTN_Task 2", 256, NULL, 1, NULL);
+    gpio_init(BTN_PIN_G);
+    gpio_set_dir(BTN_PIN_G, GPIO_IN);
+    gpio_pull_up(BTN_PIN_G);
+    gpio_set_irq_enabled_with_callback(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true,
+        &btn_callback_G);
+
+
+
     vTaskStartScheduler();
 
     while (true)
